eb51ad11d0078ffdf4b4204e2c6591f1f302f839
backen <bochen.mateusz@gmail.com>
2023-02-04 10:30:39 +0100

    Update whats new

--- frontend/src/Containers/WhatsNew/WhatsNew.jsx
+++ frontend/src/Containers/WhatsNew/WhatsNew.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import './style.css';
 
 class WhatsNew extends Component {
     render() {
@@ -8,6 +8,33 @@ class WhatsNew extends Component {
                 <h1>
                     It's works! new data base manager
                 </h1>
+
+                <h2>
+                    Features and Todo:
+                </h2>
+                <ol>
+                    <li className="feature done">
+                        Selecting query history
+                    </li>
+                    <li className="feature done">
+                        Pagination
+                    </li>
+                    <li className="feature done">
+                        Table sticky header not not jumping
+                    </li>
+                    <li className="bug">
+                        Bug: with refresh page with load database tables - all database have same tables
+                    </li>
+                    <li className="feature">
+                        Update data in double click
+                    </li>
+                    <li className="feature">
+                        Resizable query input
+                    </li>
+                    <li className="bug">
+                        Bug: show proces list not working - loading forever
+                    </li>
+                </ol>
             </div>
         );
     }
